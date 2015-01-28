5b3b19cff8549f68a5eb1860b115c50a12ad54fa
Peter Hirt <peter@awesome39.com>
2015-01-29 00:26:55 +0200

    Improved working with branhching operators. Tested.

--- FAN_Control_Tyny5/FAN_Control_Tyny5.asm
+++ FAN_Control_Tyny5/FAN_Control_Tyny5.asm
@@ -7,11 +7,11 @@
 
 .INCLUDE "tn5def.inc"
 	
-	.def		ADCresult=	R17
-	.def		Tcur=		R18
-	.def		Tprev=		R19
-	.def		Scur=		R20
-	.def		Stat=		R21
+	.def		ADCresult=	R17			
+	.def		Tcur=		R18			;; Current temperature
+	.def		Tprev=		R19			;; Previous temperature
+	.def		Scur=		R20			;; Current fan speed
+	.def		Stat=		R21			;; Flag: 0x00 - Idle, 0x01 - Cooling
 	.equ		Load=		PINB0
 	.equ		Fan=		PINB1
 	.equ		Thrm=		PINB2
@@ -58,7 +58,7 @@ init:
 	; ADC
 	ldi		R16,		0b10
 	out		ADMUX,		R16
-	ldi		R16,		(1 << ADEN)|(0b100 << ADPS0)|(1 << ADIE)
+	ldi		R16,		(1 << ADEN)|(0b111 << ADPS0)|(1 << ADIE)
 	out		ADCSRA,		R16
 	ldi		R16,		(1 << SM0)
 	out		SMCR,		R16						;	Sleep mode is ADC noise reduction
@@ -89,24 +89,19 @@ calculation:
 
 comprasion_1:
 	sbi		PORTB,		Load
-	mov		R16,		Tcur
-	cpi		R16,		Tmin
-	;; if >
-	; rjmp		speed_Up
+	ldi		R16,		Tmin
+	cp		R16,		Tcur
+	brlo	speed_Up		; if Tmin < Tcur
 	;; else
-	; rjmp		measure_Start
+	rjmp		measure_Start
 
 comprasion_2:
-	mov		R16,		Tcur
-	cp		R16,		Tprev
-	;; if >
-	; rjmp		test_Smax
-	mov		R16,		Tprev
-	cp		R16,		Tcur
-	;; if >
-	; rjmp		speed_Down
+	cp		Tprev,		Tcur
+	brlo		test_Smax	; if Tprev < Tcur
+	cp		Tcur,		Tprev
+	brlo		speed_Down	; if Tcur < Tprev
 	;; else
-	; rjmp		measure_Start
+	rjmp		measure_Start
 
 test_Smin:
 	mov		R16,		Scur
@@ -115,17 +110,20 @@ test_Smin:
 	rjmp	measure_Start
 
 test_Smax:
+	clc
 	mov		R16,		Scur
 	cpi		R16,		Smax
 	brsh	OVERHEAT
 	
 speed_Up:
+	clc
 	ldi		Stat,		0x01
 	ldi		R16,		Step
 	add		Scur,		R16
 	rjmp	set_Speed
 
 speed_Down:
+	clc
 	subi	Scur,		Step
 	out		OCR0BL,		Scur								; set speed inline -- optimised
 	rjmp	test_Smin
